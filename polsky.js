2c5f2b7cccc829998294d9b6207a94e00579cf46
Jared Mcateer <jmcateer@cf-dev-vm.istockphoto.com>
2012-07-11 09:49:27 -0600

    Initial parser portion done.

--- polsky.js
+++ polsky.js
@@ -1,24 +1,34 @@
+/**
+ * Polsky: 
+ *
+ * Converts an Infix expression to Prefix notation 
+ */
 var Parser,
     Stack,
     LEFT = 0,
     RIGHT = 1;
 
-exports.parse = function (expr) {
-    var parser = new Parser(expr);
-
-    console.log(parser.operandStack);
-    return parser.operandStack;
-};
-
+/**
+ * Parser
+ *
+ * @param string expr The infix expression to be converted, each operator and number must be separated by a space.
+ * @return array an array ordered in (reverse) polish notion
+ */
 Parser = function (expr) {
-    var tokens = expr.split(' ');
+    var tokens;
+
+    tokens = expr.split(' ');
 
     this.stack = new Stack();
     this.output = new Stack();
 
     this.parse(tokens);
+
+    return this.output.toArray();
 };
+
 Parser.prototype = {
+    // list of operators and their weights
     operators: {
         '+': { weight: 2, assoc: LEFT },
         '-': { weight: 2, assoc: LEFT },
@@ -26,31 +36,60 @@ Parser.prototype = {
         '/': { weight: 3, assoc: LEFT },
         '^': { weight: 4, assoc: RIGHT }
     } ,
-    parse: function (tokens) {
-        var token = tokens[0],
-            op1,
-            op2;
 
+    // Parse an expression.
+    // TODO error checking
+    parse: function (tokens) {
+        while (tokens.length) {
+            this.parseToken(tokens.shift());
+        }
+
+        // If there is anything left on the stack pop them into the output.
+        while (this.stack.length) {
+            this.output.push(this.stack.pop());
+        }
+    },
+
+    parseToken: function (token) {
+        var op1, op2;
+
+        //token is a number or variable
         if (token.match(/[a-zA-Z0-9]/)) {
             this.output.push(token);
+
         } else if (this.isOperator(token)) {
-            while (this.stack.length > 0 && this.isOperator(this.stack.top())) {
+            while (this.stack.length && this.isOperator(this.stack.top())) {
                 op1 = this.operators[token];
                 op2 = this.operators[this.stack.top()];
+
                 if (
                     (op1.assoc === LEFT  && op1.weight <= op2.weight) || 
                     (op1.assoc === RIGHT && op1.weight <  op2.weight)
                 ) {
                     this.output.push(this.stack.pop());
-                    continue;
+                } else {
+                    break;
                 }
-
-                break;
             }
 
-            this.output.push(token);
+            this.stack.push(token);
+
+        } else if (token === '(') {
+            this.stack.push(token);
+
+        } else if (token === ')') {
+            while (this.stack.length && this.stack.top() !== '(') {
+                this.output.push(this.stack.pop());
+            }
+
+            if (this.stack.top() === '(') {
+                this.stack.pop();
+            } else {
+                throw new Error ('Mismatched Parens');
+            }
         }
     },
+
     isOperator: function (token) {
         return !!this.operators[token];
     }
@@ -62,3 +101,8 @@ Stack.prototype = [];
 Stack.prototype.top = function () {
     return this[this.length - 1];
 };
+Stack.prototype.toArray = function () {
+    return Array.prototype.slice.call(this);
+};
+
+exports.Parser = Parser;
--- tests/test.js
+++ tests/test.js
@@ -1,10 +1,15 @@
 var assert = require("assert");
-var polsky = require("../polsky.js");
+var p = require("../polsky.js");
 
 describe('Polsky', function () {
     describe('#parse()', function () {
-        it('should an stack of operators and operands in polish/prefix notation', function () {
-            assert.deepEqual(['+', '1', '1'], polsky.parse('( 1 + 1 ) * 5'));
+        it('should return the expression in polish/prefix notation', function () {
+            assert.deepEqual(['3'], new p.Parser('3'));
+            assert.deepEqual(['1', '1', '+'], new p.Parser('1 + 1'));
+            assert.deepEqual(['2', '5', '*', '1', '+'], new p.Parser('2 * 5 + 1'));
+            assert.deepEqual(['2', '5', '1', '+', '*'], new p.Parser('2 * ( 5 + 1 )'));
+            assert.deepEqual(['3', 'x', '*', '9', 'y', '+', '4', '/', '+'], new p.Parser('3 * x + ( 9 + y ) / 4'));
+            assert.deepEqual(['3', '4', '2', '*', '1', '5', '-', '2', '3', '^', '^', '/', '+'], new p.Parser('3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3'));
         });
     });
 });
