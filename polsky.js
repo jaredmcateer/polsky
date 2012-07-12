8c45075b0e091c4a12a223efe8e625e4d26ea40b
Jared Mcateer <jmcateer@cf-dev-vm.istockphoto.com>
2012-07-12 08:45:17 -0600

    Added simple reducer

--- polsky.js
+++ polsky.js
@@ -2,6 +2,36 @@
  * Polsky: 
  *
  * Converts an Infix expression to Prefix notation 
+ *
+ *  The tokens are processed using the Shunting Yard Algorithm:
+ *
+ *  Read Token
+ *  If token is a number/variable
+ *    add to output queue
+ *  elIf token is an operator, op1, then
+ *    while there is an operator, op2, at the top of the stack loop
+ *      if op1 is left-associative and its precedence weight is <= that of op2
+ *        OR op1 has precedence weight less than that of op2
+ *          pop o2 off stack and push o2 onto output queue
+ *    end loop
+ *    push o1 onto stack
+ *  elif token is left paren
+ *    push on to stack
+ *  elif token is right paren
+ *    while operator, o1, at top of the stack is not the matching left paren
+ *      pop o1 off stack and push it onto output queue
+ *    endloop
+ *
+ *    if operator at top of stack is not the left paren
+ *       error
+ *    else 
+ *      pop matching paren off stack and discard
+ *  else
+ *    error
+ *  endif
+ *  
+ *
+ * @TODO Error checking
  */
 var Parser,
     Stack,
@@ -40,9 +70,13 @@ Parser = function (expr) {
             return that.ast;
         },
 
-        // converts array to string with, unnecessary, parenthesis
-        print: function () {
-            return that.print(that.ast);
+        /**
+         * Formats the expression into a human readable format
+         *
+         * @param boolean reduce Optional. If true the equation will attempt simple reduction
+         */
+        print: function (reduce) {
+            return that.print(that.ast, reduce);
         }
     };
 };
@@ -50,18 +84,47 @@ Parser = function (expr) {
 Parser.prototype = {
     // list of operators and their weights
     operators: {
-        '+': { weight: 2, assoc: LEFT },
-        '-': { weight: 2, assoc: LEFT },
-        '*': { weight: 3, assoc: LEFT },
-        '/': { weight: 3, assoc: LEFT },
-        '^': { weight: 4, assoc: RIGHT }
+        '+': { 
+            weight: 2, 
+            assoc: LEFT,
+            method: function (a, b) {
+                return a + b; 
+            } 
+        },
+        '-': { 
+            weight: 2, 
+            assoc: LEFT,
+            method: function (a, b) {
+                return a - b;
+            }
+        },
+        '*': { 
+            weight: 3, 
+            assoc: LEFT,
+            method: function (a, b) {
+                return a * b;
+            }
+        },
+        '/': { 
+            weight: 3, 
+            assoc: LEFT,
+            method: function (a, b) {
+                return a / b;
+            }
+        },
+        '^': { 
+            weight: 4, 
+            assoc: RIGHT,
+            method: function (a, b) {
+                return Math.pow(a, b);
+            }
+        }
     } ,
 
     /**
      * Parses an expression
      *
      * @param array tokens the tokens to be parsed in infix notation order
-     * @TODO Error checking
      */
     parse: function (tokens) {
         while (tokens.length) {
@@ -76,32 +139,6 @@ Parser.prototype = {
 
     /**
      *  Processes an indvidual token
-     *
-     *  The token is processed using the Shunting Yard Algorithm:
-     *
-     *  If token is a number/variable
-     *    add to output queue
-     *  elIf token is an operator, op1, then
-     *    while there is an operator, op2, at the top of the stack loop
-     *      if op1 is left-associative and its precedence weight is <= that of op2
-     *        OR op1 has precedence weight less than that of op2
-     *          pop o2 off stack and push o2 onto output queue
-     *    end loop
-     *    push o1 onto stack
-     *  elif token is left paren
-     *    push on to stack
-     *  elif token is right paren
-     *    while operator, o1, at top of the stack is not the matching left paren
-     *      pop o1 off stack and push it onto output queue
-     *    endloop
-     *
-     *    if operator at top of stack is not the left paren
-     *       error
-     *    else 
-     *      pop matching paren off stack and discard
-     *  else
-     *    error
-     *  endif
      *
      *  @param string token
      */
@@ -149,19 +186,23 @@ Parser.prototype = {
      * Converts an RPN array into an Abstract Syntax Tree
      *
      * @param array tokens the RPN array
+     * @return object Returns an abstract syntax tree object
      */
     convertToAst: function (tokens) {
-        var token = tokens.pop();
+        var token = tokens.pop(),
+            node;
 
         if (token.match(NUMVAR)) {
-            return token;
+            node = token;
         } else if (token.match(OPERATOR)) {
-            return {
+            node = {
                 operator: token,
-                left: this.convertToAst(tokens),
-                right:  this.convertToAst(tokens)
+                right:  this.convertToAst(tokens),
+                left: this.convertToAst(tokens)
             };
         }
+
+        return node;
     },
 
     /**
@@ -172,14 +213,64 @@ Parser.prototype = {
      * right side first, then left side formatting the string in a readable 
      * format.
      *
-     * @param object|string node The node to stringify
+     * @param mixed node The node to stringify
+     * @param boolean reduce Optional. If true the expression will attempt reduction 
+     * @return string the formatted string
      */
-    print: function (node) {
+    print: function (node, reduce) {
+        var str,
+            left,
+            right;
+
         if (typeof node === 'string') {
-            return node;
+            str = node;
         } else {
-            return '(' + node.operator + ' ' + this.print(node.right) + ' ' + this.print(node.left) + ')'; 
+            if (reduce) {
+                left = this.print(node.left, reduce);
+                right = this.print(node.right, reduce);
+
+                node.left = left;
+                node.right = right;
+                node = this.reduce(node);
+            }
+
+            if (typeof node === 'string') {
+                str = node;
+            } else {
+                str = '(' + node.operator + ' ' + this.print(node.left) + ' ' + this.print(node.right) + ')'; 
+            }
         }
+
+        return str;
+    },
+
+    /**
+     * Extremely simple reduce
+     *
+     * This function will try an perform operation on values. If it end result is an integer
+     * then it will return the value otherwise the entire node will be returned.
+     *
+     * @param object node the node in the ast to attempt reduction on
+     * @return string|object
+     */
+    reduce: function (node) {
+        if (!node.hasOwnProperty('operator')) { return node; }
+        var left = parseInt(node.left, 10),
+            right = parseInt(node.right, 10),
+            opMethod = this.operators[node.operator].method,
+            tmp,
+            retVal = node;
+
+        if (!isNaN(left) && !isNaN(right)) {
+            tmp = opMethod(left, right);
+
+            // Only return if evaluated return is an integer
+            if (tmp % 1 === 0) {
+                retVal = tmp.toString();
+            }
+        }
+
+        return retVal;
     }
 };
 
--- tests/test.js
+++ tests/test.js
@@ -2,14 +2,25 @@ var assert = require("assert");
 var polsky = require("../polsky.js");
 
 describe('Polsky', function () {
-    describe('parsing', function () {
-        it('should return the expression in polish/prefix notation', function () {
+    describe('printing', function () {
+        it('should print the formatted expression in human readable format', function () {
             assert.deepEqual(new polsky.Parser('3').print(), '3');
             assert.deepEqual(new polsky.Parser('1 + 1').print(), '(+ 1 1)');
             assert.deepEqual(new polsky.Parser('2 * 5 + 1').print(), '(+ (* 2 5) 1)');
             assert.deepEqual(new polsky.Parser('2 * ( 5 + 1 )').print(), '(* 2 (+ 5 1))');
             assert.deepEqual(new polsky.Parser('3 * x + ( 9 + y ) / 4').print(), '(+ (* 3 x) (/ (+ 9 y) 4))');
             assert.deepEqual(new polsky.Parser('3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3').print(), '(+ 3 (/ (* 4 2) (^ (- 1 5) (^ 2 3))))');
+        });
+    });
+
+    describe('printing with reduce flag set', function () {
+        it('should print the formatted and reduced expression in human readable format', function () {
+            assert.deepEqual(new polsky.Parser('3').print(true), '3');
+            assert.deepEqual(new polsky.Parser('1 + 1').print(true), '2');
+            assert.deepEqual(new polsky.Parser('2 * 5 + 1').print(true), '11');
+            assert.deepEqual(new polsky.Parser('2 * ( 5 + 1 )').print(true), '12');
+            assert.deepEqual(new polsky.Parser('3 * x + ( 9 + y ) / 4').print(true), '(+ (* 3 x) (/ (+ 9 y) 4))');
+            assert.deepEqual(new polsky.Parser('3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3').print(true), '(+ 3 (/ 8 65536))');
         });
     });
 });
